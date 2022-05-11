1903e82ccd3fe2465080140cb39727511ebbf58b
singh-kulwant <singh.kulwant@gmx.com>
2022-05-12 01:05:38 +0200

    05-12: added tests for wallet debit

---
+++ wallet/src/test/java/com/kulsin/wallet/debit/DebitResourceTest.java
@@ -0,0 +1,141 @@
+package com.kulsin.wallet.debit;
+
+import com.kulsin.wallet.errorhandling.WalletException;
+import com.kulsin.wallet.model.WalletRequest;
+import com.kulsin.wallet.model.WalletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.kulsin.wallet.balance.ResourceTestCommon.makeMockMvc;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class DebitResourceTest {
+
+    @Mock
+    private DebitService debitService;
+    @InjectMocks
+    private DebitResource debitResource;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = makeMockMvc(debitResource);
+    }
+
+    @Test
+    void debitTest_Success() throws Exception {
+        String requestPayload = """
+                        {
+                            "playerId": 123,
+                            "amount": 5.0,
+                            "currency": "EUR",
+                            "transactionId": 999888
+                        }
+                """;
+
+        String expectedResponse = """
+                        {
+                            "playerId": 123,
+                            "balance": 5.0,
+                            "transactionId": 999888
+                        }
+                """;
+
+        WalletRequest debitRequest = new WalletRequest(13L, 5.0, "EUR", 999888L);
+        Mockito.when(debitService.debitPlayer(debitRequest))
+                .thenReturn(new WalletResponse(123L, 5.0, 999888));
+
+        var request = MockMvcRequestBuilders.post("/debit.json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestPayload);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+
+        verify(debitService, times(1)).debitPlayer(debitRequest);
+
+    }
+
+    @Test
+    void debitTest_Failure_InsufficientFunds() throws Exception {
+        String requestPayload = """
+                        {
+                            "playerId": 123,
+                            "amount": 5.0,
+                            "currency": "EUR",
+                            "transactionId": 999888
+                        }
+                """;
+
+        String expectedResponse = """
+                    {
+                        "errorMessage": "Transaction declined! player has in-sufficient funds",
+                        "errorStatus": 200
+                    }
+                """;
+
+        WalletRequest debitRequest = new WalletRequest(13L, 5.0, "EUR", 999888L);
+        Mockito.when(debitService.debitPlayer(debitRequest))
+                .thenThrow(new WalletException("Transaction declined! player has in-sufficient funds"));
+
+        var request = MockMvcRequestBuilders.post("/debit.json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestPayload);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+
+        verify(debitService, times(1)).debitPlayer(debitRequest);
+
+    }
+
+    @Test
+    void debitTest_Failure_DuplicateTransaction() throws Exception {
+        String requestPayload = """
+                        {
+                            "playerId": 123,
+                            "amount": 5.0,
+                            "currency": "EUR",
+                            "transactionId": 999888
+                        }
+                """;
+
+        String expectedResponse = """
+                    {
+                        "errorMessage": "Transaction id 999888 is not unique!",
+                        "errorStatus": 200
+                    }
+                """;
+
+        WalletRequest debitRequest = new WalletRequest(13L, 5.0, "EUR", 999888L);
+        Mockito.when(debitService.debitPlayer(debitRequest))
+                .thenThrow(new WalletException("Transaction id 999888 is not unique!"));
+
+        var request = MockMvcRequestBuilders.post("/debit.json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestPayload);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+
+        verify(debitService, times(1)).debitPlayer(debitRequest);
+
+    }
+
+}
